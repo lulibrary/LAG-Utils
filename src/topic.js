cef5a7f72fedbddf91ebaa5654c7358ff279a66b
Dom Belcher <dominic.belcher@gmail.com>
2018-04-12 14:00:04 +0100

    fixed more linter errors

--- src/topic.js
+++ src/topic.js
@@ -1,14 +1,14 @@
-const SNS = require('aws-sdk/clients/sns');
-const AWS = require('aws-sdk/global');
+require('aws-sdk/clients/sns')
+const AWS = require('aws-sdk/global')
 
 class Topic {
-  constructor(arn, region) {    
-    this.arn = arn;
-    this.sns = new AWS.SNS({ region });
+  constructor (arn, region) {
+    this.arn = arn
+    this.sns = new AWS.SNS({ region })
   }
 
   publish (data) {
-    let params = this.generateSnsMessage(data);
+    let params = this.generateSnsMessage(data)
 
     return new Promise((resolve, reject) => {
       this.sns.publish(params, (err, data) => {
@@ -17,9 +17,9 @@ class Topic {
     })
   }
 
-  generateSnsMessage(data) {
+  generateSnsMessage (data) {
     return {
-      Message: typeof(data) == "string" ? data : JSON.stringify(data),
+      Message: typeof (data) === 'string' ? data : JSON.stringify(data),
       TargetArn: this.arn
     }
   }
