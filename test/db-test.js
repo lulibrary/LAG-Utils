97c5767893b46b16b7799402ce3b22b31744b62a
Dom Belcher <dominic.belcher@gmail.com>
2018-04-17 09:38:08 +0100

    fixed linter errors

--- test/db-test.js
+++ test/db-test.js
@@ -3,11 +3,11 @@ const sinon = require('sinon')
 
 // Test libraries
 const chai = require('chai')
-const chai_as_promised = require('chai-as-promised')
-chai.use(chai_as_promised)
-const sinon_chai = require('sinon-chai')
-chai.use(sinon_chai)
-const should = chai.should()
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const sinonChai = require('sinon-chai')
+chai.use(sinonChai)
+chai.should()
 
 const sandbox = sinon.sandbox.create()
 
