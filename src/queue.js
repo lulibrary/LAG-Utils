d8e76f2ffbfdc0d8a4e499e7b0f6ef1980863614
Dom Belcher <dominic.belcher@gmail.com>
2018-07-05 16:00:45 +0100

    simplified queue methods

--- src/queue.js
+++ src/queue.js
@@ -15,31 +15,30 @@ class Queue {
       QueueOwnerAWSAccountId: this.owner
     }
 
-    return new Promise((resolve, reject) => {
-      this.sqs.getQueueUrl(params, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          this.url = data.QueueUrl
-          resolve()
-        }
+    return this.sqs.getQueueUrl(params)
+      .promise()
+      .then(data => {
+        this.url = data.QueueUrl
       })
-    })
   }
 
   sendMessage (message) {
-    return (this.url
-      ? Promise.resolve()
-      : this.getQueueUrl()
-        .catch(e => {
-          throw new Error('Unable to get Queue URL')
-        }))
+    return this._url()
       .then(() => {
         return this.sqs.sendMessage({
           MessageBody: message,
           QueueUrl: this.url
         }).promise()
       })
+  }
+
+  _url () {
+    return this.url
+      ? Promise.resolve()
+      : this.getQueueUrl()
+        .catch(e => {
+          throw new Error('Unable to get Queue URL')
+        })
   }
 }
 
